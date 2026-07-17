d58a43f41a8a3930b2b8f227b32422562ee911e9
nnakhostin-lab <nnakhostin@gmail.com>
2026-07-17 15:45:40 +0330

    Update ServerComponent.jsx

--- src/app/components/ServerComponent.jsx
+++ src/app/components/ServerComponent.jsx
@@ -1,6 +1,6 @@
 async function fetchProducts(){
   //const response = await fetch("https://fakestoreapi.com/products",{cache: "no-store",});
-  const response = await fetch("httPS://jsonplaceholder.tyicode.com/posts");
+  const response = await fetch("httPS://jsonplaceholder.typicode.com/posts");
   if(!response.ok) {
     throw new Error(`HTTP Error ${response.status}`);
   }
