25d794f0f755f487af98c87f426e0c4669673d99
nnakhostin-lab <nnakhostin@gmail.com>
2026-07-17 16:27:57 +0330

    Add user component

---
+++ src/app/components/UserServerComponent.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+async function fetchUsers(){
+  const response = await fetch("https://reqres.in/api/users?page=1",{cache: "no-store",});
+  if(!response.ok) {
+    throw new Error(`HTTP Error ${response.status}`);
+  }
+  let data = await response.json()
+  return data
+}
+
+
+export default async function UserServerComponent(){
+  
+  const users =  await fetchUsers() 
+  
+  return(
+    <div>
+      <ul>
+        {
+         products.map(
+          (user) => (
+            <li key={user.id}>
+              <p> {user.first_name} {user.last_name}</p> 
+              <Image
+                src={user.avatar}
+                alt={user.first_name} 
+                width={100}
+                height={100}
+              />
+            </li>           
+          )
+         )
+        }
+
+      </ul>
+
+    </div>
+  )
+}
--- src/app/usecomponents/page.jsx
+++ src/app/usecomponents/page.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import ServerComponent from "../components/ServerComponent"
 import ClientComponent from "../components/ClientComponent";
+import UserServerComponent from "../components/UserServerComponent";
 
 export default function UseComponents() {
   return (
     <div>
       <h1>   پروژه فروشگاه</h1>
       <ServerComponent/>
+      <UserServerComponent/>
       <ClientComponent/>  
     </div>
   )
